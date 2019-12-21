7e52353192519d232523eeac0f87e5f57ec39664
mkajitani15 <m.kajitani.15@gmail.com>
2019-12-22 01:56:44 +0900

    edit_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'index' => 'index#index'
+  get "/user_identification/new" => "user_identification#new"
   post "/user_identification/create" => "user_identification#create"
 end
