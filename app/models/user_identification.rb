2a4b3ab693a34e4228475bc54068c0cc717c4e99
mkajitani15 <m.kajitani.15@gmail.com>
2019-12-27 20:08:53 +0900

    edit_modelfile
    add validation

--- app/models/user_identification.rb
+++ app/models/user_identification.rb
@@ -1,2 +1,9 @@
 class UserIdentification < ApplicationRecord
+  number = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i
+  validates :user_id, presence: true
+  validates :post_number, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :black, presence: true
+  validates :telnumber, presence: true
 end
