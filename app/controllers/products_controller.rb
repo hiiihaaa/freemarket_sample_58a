18f11d588bae65ab297fecd85ff411f0203fecc6
Funchuun <kobf1w07@gmail.com>
2019-12-29 14:08:34 +0900

    コントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,11 @@ class ProductsController < ApplicationController
     @products = Product.find(params[:id])
   end
 
+  private
   def set_product
-    @products = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
-  private
   def product_params
     params.require(:product).permit(:name, :size, :status, :shipping_fee, :shipping_method, :shipping_from, :shipping_date, :price, :brand, :purchase_status, images: [])
   end
