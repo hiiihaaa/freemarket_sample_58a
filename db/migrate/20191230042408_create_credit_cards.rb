17a47a8116319083492453eb5ba89359c4aa99b7
hiiihaaa <snshiiihaaa@gmail.com>
2019-12-30 18:26:23 +0900

    credit_card__front&back

---
+++ db/migrate/20191230042408_create_credit_cards.rb
@@ -0,0 +1,10 @@
+class CreateCreditCards < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :credit_cards
+    create_table :credit_cards do |t|
+      t.integer :user_id, foreign_key: true
+      t.string :customer_id, null: false
+      t.string :card_id, null: false
+    end
+  end
+end
