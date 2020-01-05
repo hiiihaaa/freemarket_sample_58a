97b353bfa434ffea0dcf7441c8543ab54857562b
TARACHIN <yo.hi.0308@gmail.com>
2020-01-05 12:42:18 +0900

    migrate update

--- db/migrate/20191230042408_create_credit_cards.rb
+++ db/migrate/20191230042408_create_credit_cards.rb
@@ -1,6 +1,5 @@
 class CreateCreditCards < ActiveRecord::Migration[5.2]
   def change
-    drop_table :credit_cards
     create_table :credit_cards do |t|
       t.integer :user_id, foreign_key: true
       t.string :customer_id, null: false
