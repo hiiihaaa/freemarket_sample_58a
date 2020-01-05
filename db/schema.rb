59f9fa34d139efed20086dcdf0ee7f99d3ab8cda
TARACHIN <yo.hi.0308@gmail.com>
2020-01-05 12:07:11 +0900

    Gemfile.lock update

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,6 @@ ActiveRecord::Schema.define(version: 2019_12_18_182929) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "customer_id"
-    t.index ["customer_id"], name: "index_users_on_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
