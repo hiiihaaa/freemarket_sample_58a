bc2d97d66739b18692940422e5c42de9e0fa942e
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-01-07 01:38:16 +0900

    Revert "test"
    This reverts commit acba4e0e653e6a68bd4aaa7dc2d81dbc2f1a3e77.

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_042408) do
     t.string "brand"
     t.string "purchase_status"
     t.integer "category_id"
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -68,4 +67,64 @@ ActiveRecord::Schema.define(version: 2019_12_30_042408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "product_id"
+    t.string "name"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_likes_on_product_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "product_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "product_id"
+    t.string "image_url"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "size"
+    t.string "status"
+    t.string "shipping_fee"
+    t.string "shipping_method"
+    t.string "shipping_from"
+    t.string "shipping_date"
+    t.integer "price"
+    t.string "brand"
+    t.string "purchase_status"
+    t.integer "user_id"
+    t.text "product_detail"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "firstname_kana", null: false
+    t.string "lastname_kana", null: false
+    t.datetime "birthday", null: false
+    t.string "image"
+  end
+
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "products"
+  add_foreign_key "likes", "users"
 end
