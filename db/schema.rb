2295a398cb1eefad6a60d864307a6319026176e2
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-01-06 17:38:53 +0900

    Revert "コンフリクト対策"
    This reverts commit d4e27f43489eb66d0510d29c7a0446aeda6518fd.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_12_30_042408) do
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "product_id"
     t.string "name"
