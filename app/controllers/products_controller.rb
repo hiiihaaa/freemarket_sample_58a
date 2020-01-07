9f8316f1e452e4b7c193a456cb344f679a0c4141
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-01-07 17:41:37 +0900

    コメントアウト消去

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,6 @@
 class ProductsController < ApplicationController
   def index
     @product = Product.find(1)
-    # @product = Product.find(params[:id])
     @user = User.find(@product.user_id)
     @product_image = ProductImage.find_by(product_id: @product.id)
     @category = Category.find_by(product_id: @product.id)
@@ -9,12 +8,6 @@ class ProductsController < ApplicationController
     @product_user_other_image = ProductImage.where(product_id: @product_user_other)
     @category_same = Category.where(name: @category.name)
     @other_product_image = ProductImage.where(product_id: @category_same)
-    # @product_category_other = Product.where(id: @category_same[0])
-    # @category_same.each do |product_id|
-    #   @product_category_other = Product.where(id: @category_same[product_id])
-    # end
-    # @product_category_other = Product.where(id: @category_same[0].product_id)
-    # @product_category_other = Product.where(id: @category_same.product_id)
   end
 
   def new
