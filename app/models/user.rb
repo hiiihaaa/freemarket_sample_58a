0d197e36204b36a50e025cb53c90f61c4d8f51cf
hndsfohnd <g532848_nj@yahoo.co.jp>
2020-01-09 21:30:16 +0900

    User登録機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          number = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i
-         validates :password, presence: true , format: { with: number }
+         validates :password, presence: true,length: { minimum: 7 }  , format: { with: number }
          validates :lastname, presence: true
          validates :firstname, presence: true
          validates :firstname_kana, presence: true
          validates :lastname_kana, presence: true
          validates :birthday, presence: true
+         validates :email, presence: true
 end
