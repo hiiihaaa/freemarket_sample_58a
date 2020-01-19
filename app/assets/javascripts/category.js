004460254ee02c9251c1199a6d56be1299195c12
yajiii <y.kumiko22@gmail.com>
2020-01-19 22:18:26 +0900

    レビュー修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,43 +1,43 @@
   $(function(){
-    const kodomo = document.getElementById("kodomo");
-    const mago = document.getElementById("mago");
+    const child_js = document.getElementById("child_js");
+    const grandchild_js = document.getElementById("grandchild_js");
     const size = document.getElementById("size");
     const brand = document.getElementById("brands");
-    kodomo.style.display ="none";
-    mago.style.display ="none";
+    child_js.style.display ="none";
+    grandchild_js.style.display ="none";
     size.style.display ="none";
     brand.style.display ="none";
 
     $('#c_parents').on('change', function(e){
-      var p = document.getElementById("c_parents").value;
-      if( p != ""){
+      var c_parents = document.getElementById("c_parents").value;
+      if( c_parents != ""){
         $.ajax({
         type: 'GET',
         url: '/products/cate_children',
         dataType: 'json',
-        data: {parent: p}
+        data: {parent: c_parents}
       })
 
       .done(function(child){ 
-        kodomo.style.display ="block";
+        child_js.style.display ="block";
         child.forEach(function(child){
-        var c_html = "";
-        var c_html = ($("<option>").val($(child).attr('id')).text($(child).attr('layer')));
-        $("#kodomo").append(c_html);
+        var child_html = "";
+        var child_html = ($("<option>").val($(child).attr('id')).text($(child).attr('layer')));
+        $("#child_js").append(child_html);
         })
     })
-  }else if(p == ""){
-    kodomo.style.display ="none";
-    mago.style.display ="none";
-    $("#mago").remove();
-    $("#kodomo").remove();
+  }else if(c_parents == ""){
+    child_js.style.display ="none";
+    grandchild_js.style.display ="none";
+    $("#grandchild_js").remove();
+    $("#child_js").remove();
   }
 })
-$('#kodomo').on('change', function(e){
+$('#child_js').on('change', function(e){
   
-  var id = document.getElementById("kodomo").value;
+  var id = document.getElementById("child_js").value;
   if( id != "---"){
-    mago.style.display ="block";
+    grandchild_js.style.display ="block";
     
 $.ajax({
     type: 'GET',
@@ -46,20 +46,20 @@ $.ajax({
     data: {child_id: id}
   })
   .done(function(grand_child){ 
-    mago.style.display ="block";
+    grandchild_js.style.display ="block";
     size.style.display ="block";
     brand.style.display ="block";
     grand_child.forEach(function(grand_child){
-    var g_html = "";
-    var g_html = ($("<option>").val($(grand_child).attr('id')).text($(grand_child).attr('layer')));
-    $("#mago").append(g_html);
+    var grandchild_html = "";
+    var grandchild_html = ($("<option>").val($(grand_child).attr('id')).text($(grand_child).attr('layer')));
+    $("#grandchild_js").append(grandchild_html);
     })
-    $('#kodomo').on('change', function(e){
-      $("#mago").remove();
+    $('#child_js').on('change', function(e){
+      $("#grandchild_js").remove();
     });
   });
 }else if(id == "---"){
-  mago.style.display ="none";
-  $("#mago").remove();
+  grandchild_js.style.display ="none";
+  $("#grandchild_js").remove();
 }})
 });
