847bfdfa03f05fb9b5ed04bd0f8465855680268f
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-01-22 01:53:45 +0900

    Revert "Revert "修正""
    This reverts commit 35f1043c125d212bec6b0c4e4cfac98c2f172c5f.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,16 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def show
+    @product = Product.find(params[:id])
+    @user = User.find(@product.user_id)
+    @product_image = ProductImage.find_by(product_id: @product.id)
+    @category = Category.find_by(product_id: @product.id)
+    @product_user_other = Product.where.not(id: @product.id).where(user_id: @product.user_id)
+    @product_user_other_image = ProductImage.where(product_id: @product_user_other)
+    @category_same = Category.where.not(product_id: @product.id).where(name: @category.name)
+    @other_product_image = ProductImage.where(product_id: @category_same)
+    # @next_and_back = 
   end
 
   def destroy
