bec1e09dade72eca760483933a1e10d21b01d81c
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-01 13:51:38 +0900

    Revert

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,32 @@
 class Product < ApplicationRecord
-  belongs_to :user
   has_many_attached :images
   belongs_to :user
   has_many :comments
   has_many :categories
+  has_one    :category
+  accepts_nested_attributes_for :category
+  has_one    :status
+  accepts_nested_attributes_for :status
+  has_one    :send_area
+  accepts_nested_attributes_for :send_area
+  has_one    :period
+  accepts_nested_attributes_for :period
+  has_one    :charge_method
+  accepts_nested_attributes_for :charge_method
+  has_one    :size
+  accepts_nested_attributes_for :size
+  has_one    :image
+  accepts_nested_attributes_for :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :address
+  belongs_to_active_hash :status
+
+  # belongs_to :user
+  # has_many_attached :images
+  # belongs_to :user
+  # has_many :comments
+  # has_many :categories
+
 
   def previous
     Product.where("id < ?", self.id).order("id DESC").first
