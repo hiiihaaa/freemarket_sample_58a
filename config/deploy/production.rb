7191653c93e6af21964da18e76e0770c63e2e5b4
TARACHIN <yo.hi.0308@gmail.com>
2020-02-09 14:34:48 +0900

    production.rb編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server '3.114.103.126', user: 'ec2-user', roles: %w{app db web}
 
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
-end
+# before_exec do |server|
+#   ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+# end
