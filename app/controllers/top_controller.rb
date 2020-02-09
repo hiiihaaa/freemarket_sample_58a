10e6db115db9e00a0ad1a62f33884ecb93fbc57f
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-09 17:37:07 +0900

    コメントアウト削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,9 +5,5 @@ class TopController < ApplicationController
     @mens = Product.where(category_id: 208..353)
     @electricals = Product.where(category_id: 903..988)
     @toys = Product.where(category_id: 690..802)
-    # @ladies_image = ProductImage.find_by(product_id: @ladies)
-    # @mens_image = ProductImage.find_by(product_id: @mens)
-    # @electricals_image = ProductImage.find_by(product_id: @electricals)
-    # @toys_image = ProductImage.find_by(product_id: @toys)
   end
 end
