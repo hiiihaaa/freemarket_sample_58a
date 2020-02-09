6c108e9d3b68e3ccf17c30a990a634d9599efc90
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-09 18:39:57 +0900

    Revert "Merge pull request #76 from hiiihaaa/user-profiles"
    This reverts commit 5ed793598fb607cfb8dcdccc6cffb7ceedf55d41, reversing
    changes made to 556320e45cd3609fbd3adda76076a298dcbaa386.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:destroy]
+  before_action :set_product, only: [:show, :destroy]
 
   def purchase
   end
@@ -8,18 +8,20 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
     @user = User.find(@product.user_id)
     @product_image = ProductImage.find_by(product_id: @product.id)
-    @category = Category.find_by(product_id: @product.id)
+    @category = Category.find_by(id: @product.category_id)
     @product_user_other = Product.where.not(id: @product.id).where(user_id: @product.user_id)
     @product_user_other_image = ProductImage.where(product_id: @product_user_other)
-    @category_same = Category.where.not(product_id: @product.id).where(name: @category.name)
+    @charge_method = ChargeMethod.find_by(id: @product.bearsize_id)
+    @charge_method2 = ChargeMethod.find_by(id: @product.sendmethod_id)
+    @category_same = Product.where.not(id: @product.id).where(category_id: @product.category_id)
     @other_product_image = ProductImage.where(product_id: @category_same)
   end
 
   def destroy
     @product.destroy if @product.user_id == current_user.id
+    redirect_to root_path
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,11 @@
 class UsersController < ApplicationController
-  before_action :move_to_signin
-  
-  def move_to_signin
-    redirect_to user_session_path unless user_signed_in?
+  def new
   end
-  
   def index
   end
-
+  def destroy
+  end
   def show
     redirect_to controller: 'devise/sessions', action: 'new'
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    user = User.find(params[:id])
-    if user.id == current_user.id
-      user.update_columns(user_params.to_hash)
-      redirect_to edit_user_path(current_user.id)
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:nickname,:profiles)
   end
 end
