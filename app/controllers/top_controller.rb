dbc6c3d14af8d144c599e2dd0c6b65e9ddaa0c35
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-09 15:34:16 +0900

    仕様変更及び画像表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,13 @@
 class TopController < ApplicationController
   def index
     @products = Product.limit(10)
+    @ladies = Product.where(category_id: 1..207)
+    @mens = Product.where(category_id: 208..353)
+    @electricals = Product.where(category_id: 903..988)
+    @toys = Product.where(category_id: 690..802)
+    @ladies_image = ProductImage.find_by(product_id: @ladies)
+    @mens_image = ProductImage.find_by(product_id: @mens)
+    @electricals_image = ProductImage.find_by(product_id: @electricals)
+    @toys_image = ProductImage.find_by(product_id: @toys)
   end
 end
