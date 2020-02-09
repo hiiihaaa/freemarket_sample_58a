41ea4d43d026042febbae237d1fe49f61ff01d5a
hndsfohnd <g532848_nj@yahoo.co.jp>
2020-02-09 15:48:16 +0900

    user_profiles controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:firstname_kana,:lastname_kana,:birthday,:image,:customer_id])
+    devise_parameter_sanitizer.permit(:update, keys: [:nickname,:profiles])
 end
 end
