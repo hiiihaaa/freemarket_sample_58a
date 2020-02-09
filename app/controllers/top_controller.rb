e3097b78ad40792a11023760d4dd0dfcb5e4861b
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-09 17:45:57 +0900

    limit追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,8 @@ class TopController < ApplicationController
   def index
     @products = Product.limit(10)
     @ladies = Product.where(category_id: 1..207).limit(10)
-    @mens = Product.where(category_id: 208..353)
-    @electricals = Product.where(category_id: 903..988)
-    @toys = Product.where(category_id: 690..802)
+    @mens = Product.where(category_id: 208..353).limit(10)
+    @electricals = Product.where(category_id: 903..988).limit(10)
+    @toys = Product.where(category_id: 690..802).limit(10)
   end
 end
