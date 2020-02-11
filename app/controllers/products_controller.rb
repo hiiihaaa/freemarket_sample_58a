84ee0d422dd9238b466fc84ab803e888ad973a50
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-11 17:24:56 +0900

    購入機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,32 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :destroy]
 
   def purchase
+    @product = Product.find(params[:id])
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    @product_image = ProductImage.where(product_id: @product.id)
+    #登録された情報がない場合にカード登録画面に移動
+    if credit_card.blank?
+      redirect_to new_credit_card_path
+    else
+      #保管した顧客IDでpayjpから情報取得
+      Payjp.api_key = "sk_test_0d9443b9a1a637b9456b6f24"
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
+      @default_card_information = customer.cards.retrieve(credit_card.card_id)
+    end
+  end
+  def pay
+    @product = Product.find(params[:id])
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    Payjp.api_key = 'sk_test_0d9443b9a1a637b9456b6f24'
+     payjp = Payjp::Charge.create(
+    :amount => @product.price,            #支払金額
+    # :card  => params['payjp-token'],
+    :customer => credit_card.customer_id, #顧客ID
+    :currency => 'jpy',                   #単位：日本円
+    :description => '決済に関する説明'
+    )
+    redirect_to root_path
   end
   
   def index
