607b67c59082fd6f68fce7a31c071cbf362e7a58
TARACHIN <yo.hi.0308@gmail.com>
2020-02-11 19:53:10 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -8,5 +23,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:firstname_kana,:lastname_kana,:birthday,:image,:customer_id])
     devise_parameter_sanitizer.permit(:update, keys: [:nickname,:profiles])
-end
+  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 server '3.114.103.126', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # before_exec do |server|
 #   ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
 # end
