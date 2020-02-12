fedbfd26349ba50b2c8e05ad843aa505a35bdd0c
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-12 21:27:05 +0900

    Revert "Merge pull request #84 from hiiihaaa/商品出品_修正()"
    This reverts commit db00a9797f9c121c46b578731db00c2c618ec981, reversing
    changes made to 607b67c59082fd6f68fce7a31c071cbf362e7a58.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,7 @@ class Product < ApplicationRecord
   belongs_to :period,   optional: true
   belongs_to :charge_method, optional: true
 
-
-  has_many  :product_images, dependent: :destroy
+  has_many  :product_images
   accepts_nested_attributes_for :product_images
 
   belongs_to_active_hash :address
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,4 +1,4 @@
 class ProductImage < ApplicationRecord
-  belongs_to :product, optional: true
+  belongs_to :product
   mount_uploader :image_url, ProductImagesUploader
 end
