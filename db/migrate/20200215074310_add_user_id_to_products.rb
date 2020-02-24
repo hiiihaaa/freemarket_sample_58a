936f9d1ca915a169fac8b9b1236546e2cbce0aec
TARACHIN <yo.hi.0308@gmail.com>
2020-02-24 15:15:26 +0900

    Update 20200215074310_add_user_id_to_products.rb

--- db/migrate/20200215074310_add_user_id_to_products.rb
+++ db/migrate/20200215074310_add_user_id_to_products.rb
@@ -1,12 +1,12 @@
 class AddUserIdToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_column :products, :size_id, :integer
-    add_column :products, :status_id, :integer
-    add_column :products, :bearsize_id, :integer
-    add_column :products, :sendmethod_id, :integer
-    add_column :products, :address_id, :integer
-    add_column :products, :period_id, :integer
-    add_column :products, :user_id, :integer
-    add_column :products, :charge_method_id, :integer
+    # add_column :products, :size_id, :integer
+    # add_column :products, :status_id, :integer
+    # add_column :products, :bearsize_id, :integer
+    # add_column :products, :sendmethod_id, :integer
+    # add_column :products, :address_id, :integer
+    # add_column :products, :period_id, :integer
+    # add_column :products, :user_id, :integer
+    # add_column :products, :charge_method_id, :integer
   end
 end
